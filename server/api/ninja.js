aff6b6362c603c723e1fb3b592c0dcf0e1be930d
liamjken <liamjken@gmail.com>
2023-09-14 11:34:46 -0400

    first commit

---
+++ server/api/ninja.js
@@ -0,0 +1,18 @@
+export default defineEventHandler(async (event) => {
+
+
+// handle query params
+
+// const { name } = getQuery(event)
+
+// //handle post data
+
+// const { age } = await readBody(event)
+
+//api call with private key
+const { data } = await $fetch('https://api.currencyapi.com/v3/latest?apikey=cur_live_6dozQh2NzBggiKFbTYhVNSoZSoCYFW200LFItXb2')
+
+    return data
+
+})
+
